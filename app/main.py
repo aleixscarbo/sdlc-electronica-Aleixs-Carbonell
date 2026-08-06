2d4b85aed568435268f4d3fa36a19d1d877c478e
Aleixs carbonell Montaño <aleixs2005@gmail.com>
2026-08-06 15:22:24 -0600

    feat: agregar endpoint /health para pasar el healthcheck de render

--- app/main.py
+++ app/main.py
@@ -6,6 +6,13 @@ from app.routers import readings, sensors
 # 1. Inicializar la app
 app = FastAPI(title="SensorHub API", version="1.0.0")
 
+# ---> INYECCIÓN DEL HEALTH CHECK PARA RENDER <---
+@app.get("/health")
+def health_check():
+    """Endpoint para que la nube de Render sepa que la API está viva."""
+    return {"status": "ok"}
+# ------------------------------------------------
+
 # 2. Soldar los pines (Registrar los routers)
 app.include_router(sensors.router)
 app.include_router(readings.router)
